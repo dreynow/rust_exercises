ebcc816d8a0003f59ba7a07da51d24cc0c0786db
dare sunday <dare.sunday@outlook.com>
2024-12-23 18:40:59 +0000

    minigrep added

---
+++ minigrep/target/debug/deps/minigrep.d
@@ -0,0 +1,5 @@
+C:\Users\DareSunday\Documents\projects\rust_exercises\minigrep\target\debug\deps\minigrep.exe: src\main.rs
+
+C:\Users\DareSunday\Documents\projects\rust_exercises\minigrep\target\debug\deps\minigrep.d: src\main.rs
+
+src\main.rs:
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+C:\Users\DareSunday\Documents\projects\rust_exercises\minigrep\target\debug\minigrep.exe: C:\Users\DareSunday\Documents\projects\rust_exercises\minigrep\src\main.rs
